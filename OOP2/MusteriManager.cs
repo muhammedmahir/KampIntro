c958c75cc1edb0de3827a1fbaf59cf3eafa18ea5
mahir_6161 <mahir_6161@hotmail.com>
2021-01-30 18:57:52 +0300

    30.01.2021 ders 5

---
+++ OOP2/MusteriManager.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace OOP2
+{   // iş sınıfları, 
+    class MusteriManager
+    {
+        public void Ekle(Musteri musteri)
+        {
+
+        }
+    }
+}
---
+++ OOP2/Program.cs
@@ -0,0 +1,32 @@
+﻿using System;
+
+namespace OOP2
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Engin Demirog
+            GercekMusteri musteri1 = new GercekMusteri();
+            musteri1.Id = 1;
+            musteri1.MusteriNo = "Engin";
+            musteri1.Adi = "Engin";
+            musteri1.Soyadi = "Engin";
+            musteri1.TcNo = "Engin";
+
+            TuzelMusteri musteri2 = new TuzelMusteri();
+            musteri2.Id = 2;
+            musteri2.MusteriNo = "Engin";
+            musteri2.SirketAdi = "Engin";
+            musteri2.VergiNo = "Engin";
+
+            Musteri musteri3 = new GercekMusteri();
+            Musteri musteri4 = new TuzelMusteri();
+
+            MusteriManager musteriManager = new MusteriManager();
+            musteriManager.Ekle(musteri1);
+            musteriManager.Ekle(musteri2);
+
+        }
+    }
+}
