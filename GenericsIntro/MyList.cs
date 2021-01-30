da188f19d9744e37dbd264c10525affc1e102180
mahir_6161 <mahir_6161@hotmail.com>
2021-01-30 11:28:31 +0300

    30.01.2021 4. ders

---
+++ GenericsIntro/MyList.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace GenericsIntro
+{
+    class MyList<T>
+    {
+        T[] items;
+        public MyList()
+        {
+            items = new T[0];
+        }
+        public void Add(T İtem)
+        {
+            T[] tempArray = items;
+            items = new T[items.Length + 1]; //dizinin elemn sayısını bir arttırıyorum.
+            for (int i = 0; i < tempArray.length; i++)
+            {
+                items[i] = tempArray[i];
+            }
+            items[items.Length - 1] = İtem;
+        }
+
+    }
+}
